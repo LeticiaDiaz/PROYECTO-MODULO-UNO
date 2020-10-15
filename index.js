938e59e621014fcf8e90cc275ef46ab10e039b8c
Leticia Díaz <Leticia.diaz.carmona@gmail.com>
2020-10-15 21:12:59 +0200

    fix

--- index.js
+++ index.js
@@ -7,10 +7,15 @@ let listaMarcas = [];
 let opcionesMarca = "";
 let opcionesFinales = "";
 
+eligeMarca();
+eligeProducto();
+
 function eligeMarca() {
   listaProductos = [];
   opcionesProducto = "";
-  marca = document.getElementById("brandSeleccion").value;
+  document.getElementById("resultados").innerHTML = "<h1>CARGANDO....</h1>";
+  /* marca = document.getElementById("brandSeleccion").value; */
+  console.log(url + "?brand=" + marca);
   fetch(url + "?brand=" + marca)
     .then(function (respuesta) {
       return respuesta.json();
@@ -25,17 +30,19 @@ function eligeMarca() {
         }
       }
       document.getElementById("type").innerHTML = `
-    <select onchange="eligeProducto()" id="typeSeleccion" name="type">
+    <select onchange="seleccionaTipo(event)" id="typeSeleccion" name="type">
       ${opcionesProducto}
     </select>
     `;
+      document.getElementById("resultados").innerHTML = "";
     });
 }
 
 function eligeProducto() {
   listaMarcas = [];
   opcionesMarca = "";
-  producto = document.getElementById("typeSeleccion").value;
+  /* producto = document.getElementById("typeSeleccion").value; */
+  console.log(url + "&product_type=" + producto);
   fetch(url + "?product_type=" + producto)
     .then(function (respuesta) {
       return respuesta.json();
@@ -50,7 +57,7 @@ function eligeProducto() {
         }
       }
       document.getElementById("brand").innerHTML = `
-      <select onchange="eligeMarca()" id="brandSeleccion" name="brand">
+      <select onchange="seleccionaMarca(event)" id="brandSeleccion" name="brand">
         ${opcionesMarca}
       </select>
       `;
@@ -58,16 +65,20 @@ function eligeProducto() {
 }
 
 function ensena() {
-  opcionesFinales = ""
-  marca = document.getElementById("brandSeleccion").value;
-  producto = document.getElementById("typeSeleccion").value;
+  opcionesFinales = "";
+  console.log(url + "?brand=" + marca + "&product_type=" + producto);
   fetch(url + "?brand=" + marca + "&product_type=" + producto)
     .then(function (respuesta) {
       return respuesta.json();
     })
     .then(function (datos) {
-      for (i = 0; i < datos.length; i++) {
-        opcionesFinales += `
+      console.log(datos)
+      if (datos.length == 0) {
+        document.getElementById("resultados").innerHTML =
+          "<h1>No se encuentran resultados</h1>";
+      } else {
+        for (i = 0; i < datos.length; i++) {
+          opcionesFinales += `
         <div id="producto">
         <div id="productoIMG">
         <img src="${datos[i].image_link}" alt="${datos[i].name}" />
@@ -75,10 +86,26 @@ function ensena() {
         <div id="productoDatos">
         <a href="${datos[i].product_link}" /><h3>${datos[i].name}</h3></a>
         <p>${datos[i].description}</p>
+        <a onclick="hacerFavorito()">Añadir a favorito</a>
         </div>
         </div>
         `;
+        }
+        document.getElementById("resultados").innerHTML = opcionesFinales;
       }
-      document.getElementById("resultados").innerHTML = opcionesFinales;
     });
+} 
+
+function genera() {
+  eligeMarca();
+  eligeProducto();
+}
+
+function seleccionaMarca(e) {
+  console.log(e.target.value);
+  marca = e.target.value;
+}
+function seleccionaTipo(e) {
+  producto = e.target.value;
+  console.log(producto);
 }
